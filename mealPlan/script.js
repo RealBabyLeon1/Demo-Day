181b974cbef0cb84c8ec3ec1b21689de784308d4
BabyLeanReal <realbabyleon@gmail.com>
2023-08-16 17:12:19 -0400

    why is mom

---
+++ mealPlan/script.js
@@ -0,0 +1,7 @@
+async function getFood(){
+    const response = await fetch("https://api.spoonacular.com/mealplanner/generate?apiKey=a47e2095463a46e59e67f36dcd168cbb&timeFrame=day"); // Parsing it to JSON format 
+const data = await response.json();
+console.log(data);
+}
+
+getFood()
